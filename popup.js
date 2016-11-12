88bdacacdce1b079931f8b5387aa6e998f001576
Piyush <gpiyush143@gmail.com>
2016-11-12 15:06:01 +0530

    initial commit

---
+++ popup.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	var links = Array();
+
+	//when user clicks share tabs button, get all the tab urls and store it to global 'link' variable
+	$('#shareTab').click(function(){
+		chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, function (tabs) {
+    		tabs.forEach(function(tab){
+	    		links.push(tab.url);
+		  	});
+			sendlink();
+		});
+	});
+
+	//function to send data to the server
+	function sendlink()
+	{
+		$.ajax({
+		    type: 'POST',
+		    crossDomain: true,
+		    data: {'links': JSON.stringify(links)},
+		    url: 'http://tabshare.tk/',
+		    success: function(msg) {
+		      $('#generatedLink').html(msg);
+		      $('#something').remove();
+		      $('#wrapper').css('padding-top', '20px');
+		      $('#info').removeClass('hidden');
+		      $('#generatedLink').attr('class','large');
+		    }
+		});
+		links = Array();
+	}
+
+	//fetch data from server and open tabs in a new window
+	$('#sharedlink').keydown(function(e) {
+	    if (e.keyCode == 13) {
+	        var value = $.trim($(this).val());
+	        if (value.indexOf('http://') != 0)
+    			value = 'http://' + value;
+	        $.ajax({
+			  url: value,
+			  dataType: 'json',
+			  success: function(msg){
+			  	chrome.windows.create({
+			  		url:msg
+			  	});
+			  },
+			});
+	    }
+	});
+});
